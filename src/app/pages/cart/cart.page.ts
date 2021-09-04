028881f666c1cc4a122b176a7750ddd0fe2c89bf
pasanediri97 <pasanediri@gmail.com>
2021-09-04 11:20:05 +0530

    Cart functions in the tab

--- src/app/pages/cart/cart.page.ts
+++ src/app/pages/cart/cart.page.ts
@@ -34,23 +34,217 @@ export class CartPage implements OnInit {
     private util: UtilService,
     private navCtrl: NavController,
     private chMod: ChangeDetectorRef
-  ) {
-    this.util.getCouponObservable().subscribe(data => {
-      if (data) {
-        console.log(data);
-        this.coupon = data;
-        console.log('coupon', this.coupon);
-        console.log(this.totalPrice);
-        localStorage.setItem('coupon', JSON.stringify(data));
-      }
-    });
-  }
+  ) { }
 
   ngOnInit() {
 
   }
+  getAddress() {
+    const add = JSON.parse(localStorage.getItem('deliveryAddress'));
+    if (add && add.address) {
+      this.deliveryAddress = add.address;
+    }
+    return this.deliveryAddress;
+  }
+  getVenueDetails() {
 
-  ionViewWillEnter() {
+    // Venue Details
+    this.api.getVenueDetails(this.vid).then(data => {
+      console.log(data);
+      if (data) {
+        this.name = data.name;
+        this.descritions = data.descritions;
+        this.cover = data.cover;
+        this.address = data.address;
+        this.time = data.time;
+        this.totalRatting = data.totalRatting;
+      }
+    }, error => {
+      console.log(error);
+      this.util.errorToast(this.util.translate('Something went wrong'));
+    }).catch(error => {
+      console.log(error);
+      this.util.errorToast(this.util.translate('Something went wrong'));
+    });
   }
 
+  async validate() {
+ 
+        const id = await localStorage.getItem('vid');
+        console.log('id', id);
+        if (id) {
+          this.vid = id;
+          this.getVenueDetails(); 
+          const cart = localStorage.getItem('userCart');
+          try {
+            if (cart && cart !== 'null' && cart !== undefined && cart !== 'undefined') {
+              this.cart = JSON.parse(localStorage.getItem('userCart'));
+              this.calculate();
+            } else {
+              this.cart = [];
+            }
+          } catch (error) {
+            console.log(error);
+            this.cart = [];
+          } 
+        } else {
+          this.haveItems = false;
+          this.chMod.detectChanges();
+        }
+        this.chMod.detectChanges();
+        return true;
+      }   
+
+  ionViewWillEnter() {
+    this.validate();
+  }
+  getCart() {
+    this.navCtrl.navigateRoot(['tabs/tab1']);
+  }
+  addQ(index) {
+    this.cart[index].quantiy = this.cart[index].quantiy + 1;
+    this.calculate();
+  }
+  removeQ(index) {
+    if (this.cart[index].quantiy !== 0) {
+      this.cart[index].quantiy = this.cart[index].quantiy - 1;
+    } else {
+      this.cart[index].quantiy = 0;
+    }
+    localStorage.setItem('userCart', JSON.stringify(this.foods));
+    this.calculate();
+  }
+
+  addQAddos(i, j) {
+    console.log(this.cart[i].selectedItem[j]);
+    this.cart[i].selectedItem[j].total = this.cart[i].selectedItem[j].total + 1;
+    this.calculate();
+  }
+
+  removeQAddos(i, j) {
+    console.log(this.cart[i].selectedItem[j]);
+    if (this.cart[i].selectedItem[j].total !== 0) {
+      this.cart[i].selectedItem[j].total = this.cart[i].selectedItem[j].total - 1;
+      if (this.cart[i].selectedItem[j].total === 0) {
+        const newCart = [];
+        this.cart[i].selectedItem.forEach(element => {
+          if (element.total > 0) {
+            newCart.push(element);
+          }
+        });
+        console.log('newCart', newCart);
+        this.cart[i].selectedItem = newCart;
+        this.cart[i].quantiy = newCart.length;
+      }
+    }
+    this.calculate();
+  }
+ 
+  async calculate() { 
+    let item = this.cart.filter(x => x.quantiy > 0);
+    this.cart.forEach(element => {
+      if (element.quantiy === 0) {
+        element.selectedItem = [];
+      }
+    }); 
+    this.totalPrice = 0;
+    this.totalItem = 0;
+    this.cart = []; 
+    item.forEach(element => {
+      this.totalItem = this.totalItem + element.quantiy; 
+      if (element && element.selectedItem && element.selectedItem.length > 0) {
+        let subPrice = 0;
+        element.selectedItem.forEach(subItems => {
+          subItems.item.forEach(realsItems => {
+            subPrice = subPrice + (realsItems.value);
+          });
+          subPrice = subPrice * subItems.total;
+        });
+        this.totalPrice = this.totalPrice + subPrice;
+      } else {
+        this.totalPrice = this.totalPrice + (parseFloat(element.price) * parseInt(element.quantiy));
+      }
+      this.cart.push(element);
+    });
+    localStorage.removeItem('userCart'); 
+    localStorage.setItem('userCart', JSON.stringify(this.cart));
+    this.totalPrice = parseFloat(this.totalPrice).toFixed(2); 
+
+    const tax = (parseFloat(this.totalPrice) * 21) / 100;
+    this.serviceTax = tax.toFixed(2);
+    console.log('tax->', this.serviceTax);
+    this.deliveryCharge = 5;
+    this.grandTotal = parseFloat(this.totalPrice) + parseFloat(this.serviceTax) + parseFloat(this.deliveryCharge);
+    this.grandTotal = this.grandTotal.toFixed(2);
+    if (this.coupon && this.coupon.code && this.totalPrice >= this.coupon.min) {
+      if (this.coupon.type === '%') {
+        console.log('per');
+        function percentage(num, per) {
+          return (num / 100) * per;
+        }
+        const totalPrice = percentage(parseFloat(this.totalPrice).toFixed(2), this.coupon.discout); 
+        this.dicount = totalPrice.toFixed(2);
+        this.totalPrice = parseFloat(this.totalPrice) - totalPrice; 
+        this.totalPrice = parseFloat(this.totalPrice).toFixed(2);
+        const tax = (parseFloat(this.totalPrice) * 21) / 100;
+        this.serviceTax = tax.toFixed(2); 
+        this.deliveryCharge = 5;
+        this.grandTotal = parseFloat(this.totalPrice) + parseFloat(this.serviceTax) + parseFloat(this.deliveryCharge);
+        this.grandTotal = this.grandTotal.toFixed(2);
+      } else { 
+        const totalPrice = parseFloat(this.totalPrice) - this.coupon.discout; 
+        this.dicount = this.coupon.discout;
+        this.totalPrice = totalPrice; 
+        this.totalPrice = parseFloat(this.totalPrice).toFixed(2);
+        const tax = (parseFloat(this.totalPrice) * 21) / 100;
+        this.serviceTax = tax.toFixed(2); 
+        this.deliveryCharge = 5;
+        this.grandTotal = parseFloat(this.totalPrice) + parseFloat(this.serviceTax) + parseFloat(this.deliveryCharge);
+        this.grandTotal = this.grandTotal.toFixed(2);
+      }
+    } else {
+      console.log('not satisfied');
+      this.coupon = null;
+      localStorage.removeItem('coupon');
+    }
+    console.log('grand totla', this.grandTotal);
+    if (this.totalItem === 0) {
+      const lng = localStorage.getItem('language');
+      const selectedCity = localStorage.getItem('selectedCity');
+      await localStorage.clear();
+      localStorage.setItem('language', lng);
+      localStorage.setItem('selectedCity', selectedCity);
+      this.totalItem = 0;
+      this.totalPrice = 0;
+      this.haveItems = false;
+    }
+  }
+  // NEW calc
+
+  getCurrency() {
+    return this.util.getCurrecySymbol();
+  }
+
+  changeAddress() {
+    const navData: NavigationExtras = {
+      queryParams: {
+        from: 'cart'
+      }
+    };
+    this.router.navigate(['choose-address'], navData);
+  }
+  checkout() {
+    console.log('check', this.grandTotal < 0)
+    if (this.grandTotal < 0) {
+      this.util.errorToast(this.util.translate('Something went wrong'));
+      return false;
+    }
+    const navData: NavigationExtras = {
+      queryParams: {
+        from: 'cart'
+      }
+    };
+    this.router.navigate(['choose-address'], navData); 
+  } 
 }
+
