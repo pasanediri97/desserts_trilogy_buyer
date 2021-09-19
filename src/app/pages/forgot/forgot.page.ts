22b78f6dbe1a8e1784ba331f46dcb7a64ebcfbd5
MSAthukorala <movindyaathukorala1996@gmail.com>
2021-09-19 18:53:21 +0530

    Develop the UI of Forgot password page and styling

---
+++ src/app/pages/forgot/forgot.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { forgot } from 'src/app/interfaces/forgot';
+import { NgForm } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { UtilService } from 'src/app/services/util.service';
+import { ApisService } from 'src/app/services/apis.service';
+
+@Component({
+  selector: 'app-forgot',
+  templateUrl: './forgot.page.html',
+  styleUrls: ['./forgot.page.scss'],
+})
+export class ForgotPage implements OnInit {
+
+  
+  constructor(
+    
+  ) { }
+
+  ngOnInit() {
+  }
+  
+
+}
