9405594630afcab4a34f109d1ce653ee4eb1a095
MSAthukorala <movindyaathukorala1996@gmail.com>
2021-09-19 18:54:08 +0530

    Forgot password form validation

--- src/app/pages/forgot/forgot.page.ts
+++ src/app/pages/forgot/forgot.page.ts
@@ -12,13 +12,41 @@ import { ApisService } from 'src/app/services/apis.service';
 })
 export class ForgotPage implements OnInit {
 
-  
+  login: forgot = { email: '' };
+  submitted = false;
   constructor(
-    
+    private navCtrl: NavController,
+    private util: UtilService,
+    private api: ApisService
   ) { }
 
   ngOnInit() {
   }
-  
+  onLogin(form: NgForm) {
+    console.log('form', form);
+    this.submitted = true;
+    if (form.valid) {
+      const emailfilter = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+      if (!emailfilter.test(this.login.email)) {
+        this.util.showToast(this.util.translate('Please enter valid email'), 'danger', 'bottom');
+        return false;
+      }
+      this.util.show();
+      this.api.resetPassword(this.login.email).then((data) => {
+        this.util.hide();
+        this.util.showToast(this.util.translate('Reset Password link is sent to your email'), 'dark', 'bottom');
+        console.log('sent', data);
+        this.navCtrl.back();
+      }, error => {
+        console.log(error);
+        this.util.hide();
+        this.util.showErrorAlert(this.util.translate('Something went wrong'));
+      }).catch(error => {
+        console.log(error);
+        this.util.hide();
+        this.util.showErrorAlert(this.util.translate('Something went wrong'));
+      });
+    }
+  }
 
 }
