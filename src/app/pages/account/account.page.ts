ac684529d57c876eb8d57b504c1205918635f511
sachith94-lion <sachithmanuradha@gmail.com>
2021-09-19 14:15:10 +0530

    Get user profile data functions

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -20,10 +20,76 @@ export class AccountPage implements OnInit {
     private router: Router,
     private util: UtilService
   ) {
-    
+    this.util.getReviewObservable().subscribe(data => {
+      console.log(data);
+      this.getReview();
+    });
+    this.util.observProfile().subscribe(data => {
+      this.getProfile();
+    });
   }
 
   ngOnInit() {
   }
+  logout() {
+    this.api.logout().then((data) => {
+      this.router.navigate(['tabs']);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
  
+
+  ionViewWillEnter() {
+    this.validate();
+  }
+  getReview() {
+    this.api.getMyReviews(this.id).then((reviews) => {
+      console.log(reviews);
+      this.reviews = reviews;
+    }, error => {
+      console.log(error);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  getProfile() {
+    this.api.getMyProfile(localStorage.getItem('uid')).then((data: any) => {
+      console.log('userdata', data);
+      if (data) {
+        this.name = data.fullname;
+        this.photo = data && data.cover ? data.cover : 'assets/imgs/user.jpg';
+        this.email = data.email;
+        this.id = data.uid;
+        this.getReview();
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  validate() {
+    this.api.checkAuth().then(async (user: any) => {
+      if (user) {
+        localStorage.setItem('uid', user.uid);
+        this.getProfile();
+      } else {
+        this.router.navigate(['login']);
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  changeSegment(val) {
+    this.seg_id = val;
+  }
+
+  goToselectRestaurants() {
+    this.router.navigate(['/choose-restaurant']);
+  }
+
+  goToEditProfile() {
+    this.router.navigate(['/edit-profile']);
+  }
 }
